bd9f364c4f47d23348af70b8ffd2f80a884a589f
brad winter <braddjwinter@gmail.com>
2016-01-21 07:16:37 -0800

    syntax error

--- server/config.production.js
+++ server/config.production.js
@@ -1,10 +1,16 @@
-module.exports={
-  "restApiRoot":process.env.API_URL || '/api',
-  "host":"0.0.0.0",
+// TODO - when / if moving to external API, need to parse out host and port from URL
+
+
+module.exports= {
+  "restApiRoot": process.env.API_URL || '/api',
+  "host": "0.0.0.0",
   "port": 80,
 
   "remoting": {
     "errorHandler": {
       "disableStackTrace": true
     }
+  }
 };
+
+
