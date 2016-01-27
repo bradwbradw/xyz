8e6952ceffb45278471df481a71af575c9a78037
brad winter <braddjwinter@gmail.com>
2016-01-26 17:04:30 -0800

    nicer deleting of spaces

---
+++ client/scripts/controllers/body.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('xyzApp')
+    .controller('BodyCtrl', function ($rootScope, $window, $scope) {
+
+
+        $rootScope.askConfirm = function(callback, thing, error){
+            if($window.confirm){
+                callback(thing).then(error);
+            }
+        }
+    });
+
+//askConfirm
