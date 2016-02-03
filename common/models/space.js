64abda9ae6ad37ceb8b7c3b5129b31e922bb9c13
brad winter <braddjwinter@gmail.com>
2016-02-03 08:28:10 -0800

    space playlist remote method

--- common/models/space.js
+++ common/models/space.js
@@ -1,3 +1,45 @@
-module.exports = function(Space) {
+var loopback = require('loopback');
 
+var Space = loopback.getModel('Space');
+var Song = loopback.getModel('Song');
+
+module.exports = function (Space) {
+
+
+  Space.playlist = function (spaceId, cb) {
+
+    Space.findById(spaceId,{include:'songs'},function (err,data) {
+
+      Song.find({})
+      console.log(data);
+
+      var response = {
+        space: data
+      };
+      cb(null, response);
+    })
+  };
+
+  Space.thing = function (cb) {
+    var response = 'thingy';
+    cb(null, response);
+  };
+
+  Space.remoteMethod(
+    'playlist',
+    {
+      http: {path: '/playlist', verb: 'get'},
+      accepts: {arg: 'spaceId', type: 'string'},
+      returns: {arg: 'playlist', type: 'string'}
+    }
+  );
+
+  Space.remoteMethod(
+    'thing',
+
+    {
+      http: {path: '/thing', verb: 'get'},
+      returns: {arg: 'thing', type: 'string'}
+    }
+  )
 };
