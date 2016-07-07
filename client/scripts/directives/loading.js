9927ba8f889a33a7decd9c77057f8560091a182d
brad winter <braddjwinter@gmail.com>
2016-07-07 07:49:10 -0700

    loading directive (use to show element when there are pending network requests)

---
+++ client/scripts/directives/loading.js
@@ -0,0 +1,21 @@
+/**
+ * Created by brad on 7/4/16.
+ */
+xyzApp
+  .directive('loading', ['$http', function ($http) {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        scope.isLoading = function () {
+          return $http.pendingRequests.length > 0;
+        };
+        scope.$watch(scope.isLoading, function (value) {
+          if (value) {
+            element.removeClass('ng-hide');
+          } else {
+            element.addClass('ng-hide');
+          }
+        });
+      }
+    };
+}]);
