b255de48b0389ece23d1442154dcf31bac1b972d
brad winter <braddjwinter@gmail.com>
2016-07-07 07:58:22 -0700

    mongo creds come from env var

--- constants.js
+++ constants.js
@@ -1,4 +1,18 @@
 'use strict';
+var mongoDbUriTool = require('mongodb-uri');
+
+var mongoUrl = process.env.MONGODB_URL || 'mongodb://heroku_cc7gbkr1:rusl214k9b95o5d7evobgufue6@ds059135.mongolab.com:59135/heroku_cc7gbkr1';
+
+var mongoCreds = mongoDbUriTool.parse(mongoUrl);
+/* example creds object:
+{ scheme: 'mongodb',
+  username: 'heroku_cc7gbkr1',
+  password: 'rusl214k9b95o5d7evobgufue6',
+  database: 'heroku_cc7gbkr1',
+  hosts: [ { host: 'ds059135.mongolab.com', port: 59135 } ]
+  }
+ */
+
 
 module.exports = {
   api: {
@@ -11,6 +25,7 @@ module.exports = {
     sc: process.env.SC_KEY || 'c29e4129f2bba3771a5472a65cad37e4',
     yt: process.env.YT_KEY || 'AIzaSyAv5-et2TSQ3VsA5eKLviq2KjfExzFLxO8'
   },
-  mongoUrl: process.env.MONGODB_URL || 'mongodb://heroku_cc7gbkr1:rusl214k9b95o5d7evobgufue6@ds059135.mongolab.com:59135/heroku_cc7gbkr1'
+  mongoUrl: mongoUrl,
+  mongoCreds: mongoCreds
 
 };
