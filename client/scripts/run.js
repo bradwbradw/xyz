73500cf48064f71e01e72b2c0e55d608d48da1e5
brad winter <braddjwinter@gmail.com>
2016-07-28 20:50:55 -0700

    state errors

--- client/scripts/run.js
+++ client/scripts/run.js
@@ -3,7 +3,7 @@ xyzApp.run(function($rootScope, $state, $log){
 
   $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
     event.preventDefault();
-    $state.get('error').error = { code: 123, description: 'Exception stack trace' }
+    $state.get('error').error = { code: 123, description: 'Exception stack trace' };
     $log.error(error);
 //    return $state.go('error');
   });
