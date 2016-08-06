76cbb9831d96145a421e1104ad1f1fdf85f2c15d
brad winter <braddjwinter@gmail.com>
2016-08-05 21:09:45 -0700

    fixes #1

---
+++ start.js
@@ -0,0 +1,26 @@
+var forever = require('forever');
+
+var child = new (forever.Monitor)('server/server.js', {
+  max: 3,
+  silent: false,
+  args: []
+});
+
+child.on('watch:restart', function (info) {
+  console.error('Restaring script because ' + info.file + ' changed');
+});
+
+child.on('restart', function () {
+  console.error('Forever restarting script for ' + child.times + ' time');
+});
+
+child.on('exit:code', function (code) {
+  console.error('Forever detected script exited with code ' + code);
+});
+
+
+child.on('exit', function () {
+  console.log('your-filename.js has exited after 3 restarts');
+});
+
+child.start();
