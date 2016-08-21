6bc48fda3bd03fa233de4e5a8abc39a0201b7010
brad winter <braddjwinter@gmail.com>
2016-08-21 12:50:24 -0700

    nodemon config to watch for relevent file changes and restart automatically (fixes #46)

--- dev.sh
+++ dev.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-DEBUG=loopback:* MONGODB_URL=mongodb://localhost/xyz nodemon --watch server server/server.js
+DEBUG=loopback:* MONGODB_URL=mongodb://localhost/xyz nodemon server/server.js
