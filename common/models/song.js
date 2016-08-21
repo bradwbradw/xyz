ae8910e14d483ace643778de4f5df430d3205b3e
brad winter <braddjwinter@gmail.com>
2016-08-21 15:44:39 -0700

    public boolean and setting in save hook

--- common/models/song.js
+++ common/models/song.js
@@ -1,30 +1,42 @@
-module.exports = function(Song) {
+var request = require('request-promise');
+
+var MediaApi = require('../../server/controllers/media-api.js');
+
+module.exports = function (Song) {
 
 
-/*
-    Song.observe('after save', function(ctx, next) {
+  Song.observe('before save', function (ctx, next) {
 
-        console.log('context: ',ctx)
-        if (ctx.instance) {
-            console.log('Saved %s#%s', ctx.Song, ctx.instance.id);
-        } else {
-            console.log('Updated %s matching %j',
-                ctx.Model.songs,
-                ctx.where);
-        }
-        next();
-    });
-
-
-    Song.afterRemote( 'save', function( ctx, next) {
-        console.log('*** after creating ***');
-        console.log('request: ',ctx.req);
-        console.log();
-        console.log('result: ',ctx.result);
-        console.log();
+//    console.log('context: ', ctx);
+    if (ctx.instance) {
+      var song = ctx.instance;
+      MediaApi.checkForAvailability(song)
+        .then(function(available){
+          console.log(song.title+' available? ', available);
+          song.public = available;
+        })
+        .finally(next);
+    } else {
+      console.log('Updated %s matching %j',
+        ctx.Model.songs,
+        ctx.where);
 
         next();
-    });*/
+    }
+
+
+  });
+
+  /*
+   Song.afterRemote( 'save', function( ctx, next) {
+   console.log('*** after creating ***');
+   console.log('request: ',ctx.req);
+   console.log();
+   console.log('result: ',ctx.result);
+   console.log();
+
+   next();
+   });*/
 
 
 };
