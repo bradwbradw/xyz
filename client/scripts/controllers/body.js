9cc762f203ac63164edf2200783b33c9ae4c0c0e
brad winter <braddjwinter@gmail.com>
2016-09-20 08:59:12 -0700

    add debug view

--- client/scripts/controllers/body.js
+++ client/scripts/controllers/body.js
@@ -1,31 +1,33 @@
 'use strict';
 
 angular.module('xyzApp')
-    .controller('BodyCtrl', function ($rootScope, $window, $scope, $log, $stateParams, Utility, Playlister) {
+  .controller('BodyCtrl', function ($rootScope, $window, $scope, $log, $stateParams, localStorageService, Utility, Playlister) {
 
-        $rootScope.askConfirm = function(callback, thing, error){
-            if($window.confirm){
-                callback(thing).then(error);
-            }
-        };
-      var playingSpace;
-      var playSpace = function(space){
-        Playlister.recompute(space);
-        playingSpace = space;
-      };
-
-      var getPlayingSpace = function(){
-        return playingSpace;
-      };
-
-      $scope.playSpace = playSpace;
-      $scope.getPlayingSpace = getPlayingSpace;
-      $scope.Playlister = Playlister;
-      $scope.Utility = Utility;
-
-      $rootScope.showError = function(thing){
-        $rootScope.error = Utility.cleanError(thing);
+    $rootScope.askConfirm = function (callback, thing, error) {
+      if ($window.confirm) {
+        callback(thing).then(error);
       }
-    });
+    };
+    $rootScope.debug = !_.isNull(localStorageService.get('d'));
+
+    var playingSpace;
+    var playSpace = function (space) {
+      Playlister.recompute(space);
+      playingSpace = space;
+    };
+
+    var getPlayingSpace = function () {
+      return playingSpace;
+    };
+
+    $scope.playSpace = playSpace;
+    $scope.getPlayingSpace = getPlayingSpace;
+    $scope.Playlister = Playlister;
+    $scope.Utility = Utility;
+
+    $rootScope.showError = function (thing) {
+      $rootScope.error = Utility.cleanError(thing);
+    }
+  });
 
 //askConfirm
