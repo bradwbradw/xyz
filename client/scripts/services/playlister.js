c3d9e7fb6b71b961e837f6cf51e0b4cf8d9d66d0
brad winter <braddjwinter@gmail.com>
2016-09-22 08:45:27 -0700

    fix playlist wip

--- client/scripts/services/playlister.js
+++ client/scripts/services/playlister.js
@@ -6,7 +6,7 @@ angular.module('xyzApp')
 
     var Playlister = {
       list: [],
-      getList: function(){
+      getList: function () {
         return Playlister.list;
       },
       recompute: function (space) {
@@ -36,7 +36,10 @@ angular.module('xyzApp')
             }
             distances[otherSong.id] = {
               distance: distanceBetweenItems(song, otherSong),
-              id: otherSong.id
+              id: otherSong.id,
+              x: otherSong.x,
+              y: otherSong.y,
+              name: otherSong.title
             };
           });
           distances = _.sortBy(distances, 'distance');
@@ -44,20 +47,51 @@ angular.module('xyzApp')
           return distances;
         };
 
+        // returns an array that sorts the songs into
+        // a list sorted using "nearestNeighbour"
 
-        var playlist = [];
+        var sortByNearest = function (songs, seed) {
+
+          var sorted = [];
+          var safety = 0;
+
+          var sort = function (unvisited, currentNode) {
+
+            safety++;
+            if (safety > 100) {
+              return [];
+            }
+            sorted = _.concat(sorted, currentNode);
+            if (_.size(unvisited) === 0) {
+              return;
+            }
+            var nextNodeId = _.get(_.first(distancesToOtherItems(currentNode, unvisited)), 'id');
+            var nextNode = _.first(
+              _.remove(unvisited, {id: nextNodeId})
+            );
+            $log.debug('next unvisited set is ', _.map(unvisited,'title'));
+            $log.debug('next node is ', _.get(nextNode, 'title'));
+            $log.debug('sorted is ', _.map(sorted, 'title'));
+
+            sort(unvisited, nextNode);
+          };
+
+          sort(songs, seed);
+          $log.log('all recursive should be donw');
+
+          return sorted;
+        };
 
         var songs = _.filter(space.songs, function (song) {
           return _.isUndefined(song.public) || song.public == true;
         });
 
+        songs = _.uniqBy(songs, 'id');
+
         if (_.size(songs) <= 0) {
           $log.debug('space has no songs: ', space);
           return [];
         } else {
-          _.each(songs, function (song) {
-            song.distances = distancesToOtherItems(song, songs);
-          });
 
           var seedSong;
 
@@ -69,16 +103,10 @@ angular.module('xyzApp')
             seedSong = _.first(_.sortBy(songs, 'date_saved'));
           }
 
-          playlist = [seedSong];
+          Playlister.list = sortByNearest(songs, [seedSong]);
 
-          _.each(seedSong.distances, function (song) {
-            playlist.push(_.find(songs, {'id': song.id}));
-          });
-
-          Playlister.list = _.uniqBy(playlist,'id');
-
-          $log.debug('computed playlist: ', playlist);
-          return playlist;
+          $log.debug('computed playlist: ', Playlister.list);
+          return Playlister.list;
         }
 
       }
