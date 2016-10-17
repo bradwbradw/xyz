8aef38f5f888b3d2c8449c8c80231fdc9e390bb1
brad winter <braddjwinter@gmail.com>
2016-10-17 13:44:25 -0700

    improve state error msg

--- client/scripts/run.js
+++ client/scripts/run.js
@@ -1,11 +1,10 @@
-xyzApp.run(function($rootScope, $state, $log){
+xyzApp.run(function ($rootScope, $state, $log) {
 
 
   $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
 //    event.preventDefault();
 //    $state.get('error').error = { code: 123, description: 'Exception stack trace' };
-    $log.error('error transitioning to '+toState+' from '+fromState);
-    $log.error(error);
+    $log.error('error transitioning to ' + toState.name + ' from ' + fromState.name + ':', error);
 //    return $state.go('error');
   });
 
