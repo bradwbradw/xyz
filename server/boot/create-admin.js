8dce57ed4cc61c267ccaaf5c4ff238c25853d6d1
brad winter <braddjwinter@gmail.com>
2016-10-31 15:17:11 -0700

    CREATE_ADMIN_EMAIL activated

--- server/boot/create-admin.js
+++ server/boot/create-admin.js
@@ -1,31 +1,50 @@
 var _ = require('lodash');
 
-var adminEmail = process.env.ADMIN_EMAIL;
-var adminPass = process.env.ADMIN_PASS;
-
-if (adminEmail && adminPass) {
+var adminEmail = process.env.CREATE_ADMIN_EMAIL;
 
 
-  User.create([
-    {username: 'bradmin', email: adminEmail, password: adminPass}
-  ], function (err, users) {
-    if (err) return debug('%j', err);
+module.exports = function (app) {
 
-    Role.create({
-      name: 'admin'
-    }, function (err, role) {
-      if (err) return debug(err);
-      debug(role);
+  if (adminEmail) {
 
-      // Make Bob an admin
-      role.principals.create({
-        principalType: RoleMapping.USER,
-        principalId: _.last(users).id
-      }, function (err, principal) {
-        if (err) return debug(err);
-        debug(principal);
+    var Dj = app.models.dj;
+    var Role = app.models.Role;
+    var RoleMapping = app.models.RoleMapping;
+
+    Dj.create([
+      {name: 'bradmin', email: adminEmail, password: 'admin'}
+    ], function (err, djs) {
+      if (err) {
+        if(JSON.stringify(err).indexOf('Email already exists') > -1){
+          console.log('admin user has already been created');
+        } else {
+          console.error(err);
+        }
+        return;
+      }
+      console.log('created djs', djs);
+      Role.create({
+        name: 'admin'
+      }, function (err, role) {
+        if (err) {
+          console.error(err);
+          console.error('role is: ', role);
+          return;
+        }
+
+        // Make Bob an admin
+        role.principals.create({
+          principalType: RoleMapping.USER,
+          principalId: _.last(djs).id
+        }, function (err, principal) {
+          if (err) {
+            console.error(err);
+            console.error('principal is: ', principal);
+          }
+        });
       });
     });
-  });
-}
+  }
+
+};
 
