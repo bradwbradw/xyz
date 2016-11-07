d1d7a22ef71e3cb0f5d6e1a8522c56a54f82cd73
brad winter <braddjwinter@gmail.com>
2016-11-07 13:15:53 -0800

    fix path to index file ( broke as of node 6, probably)

--- server/middleware/serve-index-html.js
+++ server/middleware/serve-index-html.js
@@ -3,10 +3,10 @@ module.exports = function () {
   return function fallback(req, res, next) {
 
     // NOTE: __dirname ends with "/server/middleware"
-    //   console.log('serving relative to dirname: ',__dirname);
+       console.log('serving relative to dirname: ',__dirname);
 
     res.sendFile('dist/index.html',{
-      root:__dirname+'../../..'
+      root:__dirname+'/../..'
     });
   };
 };
