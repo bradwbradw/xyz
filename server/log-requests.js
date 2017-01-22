b5a34e64667061a7b7a667c7b96dac1330dfe5af
brad winter <braddjwinter@gmail.com>
2017-01-22 22:05:43 +0800

    Create log-requests.js

---
+++ server/log-requests.js
@@ -0,0 +1,26 @@
+  var winston = require('winston');
+  require('winston-papertrail').Papertrail;
+
+  var winstonPapertrail = new winston.transports.Papertrail({
+    host: 'logs5.papertrailapp.com',
+    port: 29706
+  })
+
+  winstonPapertrail.on('error', function(err) {
+    console.error("error in winston: ", err);
+  });
+
+  var logger = new winston.Logger({
+    transports: [winstonPapertrail]
+  });
+
+  logger.info('this is my message');
+  
+  var logRequests = function(req, res){
+  
+      logger.info("ding");
+  
+  }
+ 
+  
+  module.exports = logRequests;
