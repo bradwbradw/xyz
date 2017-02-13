f1f69bc9edacc5c5455f70ec2d2ae9f6c44cb32a
brad winter <braddjwinter@gmail.com>
2017-02-13 13:28:49 -0800

    don't initialize until first request
    because angular loopback generator wants to require everything and the "gulp loopack" task won't finish

--- server/remote-log.js
+++ server/remote-log.js
@@ -1,28 +1,37 @@
-var winston = require('winston');
 var _ = require('lodash');
-
 _.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
-require('winston-papertrail').Papertrail;
-
 var constants = require('../constants');
 
-var winstonPapertrail = new winston.transports.Papertrail({
-  host: constants.logging.host,
-  port: constants.logging.port
-});
+var winston = require('winston');
 
-winstonPapertrail.on('error', function (err) {
-  console.error('error in winston: ', err);
-});
+require('winston-papertrail').Papertrail;
 
-var logger = new winston.Logger({
-  transports: [winstonPapertrail]
-});
+var logger;
 
+function initializeLogger() {
+
+  var winstonPapertrail = new winston.transports.Papertrail({
+    host: constants.logging.host,
+    port: constants.logging.port
+  });
+  logger = new winston.Logger({
+    transports: [winstonPapertrail]
+  });
+
+
+  winstonPapertrail.on('error', function (err) {
+    console.error('error in winston: ', err);
+  });
+
+}
 var messageTemplate = '{{ip}} {{protocol}} {{method}}: {{originalUrl}} {{params}} {{query}}';
 var messageFn = _.template(messageTemplate);
 
 var request = function (req, res, next) {
+
+  if (!logger) {
+    initializeLogger();
+  }
 
   var dataToLog = _.pick(req, 'ip protocol method originalUrl params query'.split(' '));
   dataToLog.params = JSON.stringify(dataToLog.params);//'donkey'//_(dataToLog.params).value();
@@ -33,13 +42,13 @@ var request = function (req, res, next) {
   next();
 };
 
-var error = function(err, req, res, next){
+var error = function (err, req, res, next) {
 
   logger.error('REST error:', err);
   next();
 };
 
 module.exports = {
-  request:request,
-  error:error
+  request: request,
+  error: error
 };
