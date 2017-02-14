eced1c235888c3c3b0a098ff24786874e36fc6f5
brad winter <braddjwinter@gmail.com>
2017-02-13 21:42:07 -0800

    tour loading from cms

--- client/scripts/tour.js
+++ client/scripts/tour.js
@@ -1,57 +1,64 @@
 angular.module('xyzApp')
-  .run(function ($location, $window) {
+  .run(function ($location, $window, Content) {
 
     if ($location.hash() === 'tour') {
 
-      var Sideshow = $window.Sideshow;
-      Sideshow.config.language = "en";
-      Sideshow.init();
-      Sideshow.registerWizard({
-        name: "introducing_sideshow",
-        title: "Introducing Sideshow",
-        description: "Introducing the main features and concepts of Sideshow. ",
-        estimatedTime: "10 Minutes",
-        affects: [
-          {hash: ""}, //This tutorial would be eligible for URLs like this "http://www.foo.com/bar#messages"
-          {route: "", caseSensitive: true},  //This tutorial would be eligible for URLs like this "http://www.foo.com/adm/orders"
-          function () {
-            //Here we could do any checking to infer if this tutorial is eligible the current screen/context.
-            //After this checking, just return a boolean indicating if this tutorial will be available.
-            return true;//
-          }
-        ]
-      })
-        .storyLine({
-          showStepPosition: true,
-          steps: [
-            {
-              title: "Hello Sideshow.",
-              text: "log in here",
-              subject: "#signup-login-link",
-              targets: "#signup-login-link",
+      Content.fetch()
+        .then(function (content) {
 
-              completingConditions: [
-                function () {
-                  return $location.path().indexOf('signup') >= 0;
+          var Sideshow = $window.Sideshow;
+          Sideshow.config.language = "en";
+          Sideshow.init();
+          Sideshow.registerWizard({
+            name: "introducing_sideshow",
+            title: content['tour-title'].content.md,
+            description: content['tour-subtitle'].content.md,
+            estimatedTime: "10 Minutes",
+            affects: [
+              {hash: ""}, //This tutorial would be eligible for URLs like this "http://www.foo.com/bar#messages"
+              {route: "", caseSensitive: true},  //This tutorial would be eligible for URLs like this "http://www.foo.com/adm/orders"
+              function () {
+                //Here we could do any checking to infer if this tutorial is eligible the current screen/context.
+                //After this checking, just return a boolean indicating if this tutorial will be available.
+                return true;//
+              }
+            ]
+          })
+            .storyLine({
+              showStepPosition: true,
+              steps: [
+                {
+                  title: "Hello Sideshow.",
+                  text: "log in here",
+                  subject: "#signup-login-link",
+                  targets: "#signup-login-link",
+
+                  completingConditions: [
+                    function () {
+                      return $location.path().indexOf('signup') >= 0;
+                    }
+                  ]
+
+                },
+                {
+                  title: "enter your email",
+                  text: "an email",
+                  subject: "#register-email-field",
+                  targets: "#register-email-field"
+                },
+                {
+                  title: "enter a password",
+                  text: "enter a password that has minimum of 6 characters",
+                  subject: "#register-password-field",
+                  targets: "#register-password-field"
                 }
               ]
+            });
 
-            },
-            {
-              title: "enter your email",
-              text: "an email",
-              subject: "#register-email-field",
-              targets: "#register-email-field"
-            },
-            {
-              title: "enter a password",
-              text: "enter a password that has minimum of 6 characters",
-              subject: "#register-password-field",
-              targets: "#register-password-field"
-            }
-          ]
+          Sideshow.start({listAll: true});
+
+
         });
 
-      Sideshow.start({listAll: true});
     }
   });
